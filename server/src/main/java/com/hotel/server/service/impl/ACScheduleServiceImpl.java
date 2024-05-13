b3474c7558a49da7f01385bc35954718bf75c2a7
郭晨旭 <2269409349@qq.com>
2024-05-13 12:11:02 +0800

    feat: 增加了新的调度方式

--- server/src/main/java/com/hotel/server/service/impl/ACScheduleServiceImpl.java
+++ server/src/main/java/com/hotel/server/service/impl/ACScheduleServiceImpl.java
@@ -40,13 +40,21 @@ public class ACScheduleServiceImpl implements ACScheduleService {
     // 请求调度队列
     private static final PriorityQueue<ACRequest> requestQueue = new PriorityQueue<>((o1, o2) -> {
         if (!Objects.equals(o1.getPrice(), o2.getPrice())) {
-            return ~(new BigDecimal(o1.getPrice()).compareTo(new BigDecimal(o2.getPrice())));
+            return new BigDecimal(o2.getPrice()).compareTo(new BigDecimal(o1.getPrice()));
         }
         return o1.getStartTime().compareTo(o2.getStartTime());
     });
 
+    // 服务队列
+    private static final PriorityQueue<ACRequest> runningQueue = new PriorityQueue<>((o1, o2) -> {
+        if (!Objects.equals(o1.getPrice(), o2.getPrice())) {
+            return new BigDecimal(o1.getPrice()).compareTo(new BigDecimal(o2.getPrice()));
+        }
+        return o2.getStartTime().compareTo(o1.getStartTime());
+    });
+
     // 已有调度的map
-    private static final ConcurrentHashMap<String, ACThread> runningMap = new ConcurrentHashMap<>();
+//    private static final ConcurrentHashMap<String, ACThread> runningMap = new ConcurrentHashMap<>();
 
     @DubboReference
     private CacheService cacheService;
@@ -59,71 +67,131 @@ public class ACScheduleServiceImpl implements ACScheduleService {
      */
     @Override
     public void removeOne(String userId) {
-        runningMap.remove(userId);
-        this.schedule();
+//        runningMap.remove(userId);
+        runningQueue.removeIf((e) -> userId.equals(e.getUserId()));
+        this.newSchedule();
     }
 
     @Override
     public void addOne(ACRequest acRequest) {
 //        requestQueue.add(acRequest);
         addUniqueQueue(acRequest, requestQueue);
-        this.schedule();
+        this.newSchedule();
     }
 
     /**
      * 调度
      */
-    public synchronized void schedule() {
+//    public synchronized void schedule() {
+//        String traceId = IdUtil.getSnowflakeNextIdStr();
+//        log.info("=======================调度前 {}========================", traceId);
+//        log.info("运行map: {}, 运行的空调个数: {}", runningMap, runningMap.size());
+//        log.info("调度队列: {}, 等待的空调个数: {}", requestQueue, requestQueue.size());
+//        ACProperties acProperties = (ACProperties) cacheService.get(RedisKeys.AC_PROPERTIES, ACProperties.class);
+//        // 先满足让运行空调达到最大值
+//        while (runningMap.size() < acProperties.getCount() && !requestQueue.isEmpty()) {
+//            ACRequest acRequest = requestQueue.poll();
+//            ACThread acThread = (ACThread) coolService.getACThread(acRequest.getUserId());
+//            acThread.turnOn(acRequest.getTargetTemperature(), acRequest.getChangeTemperature(),
+//                    acRequest.getStatus(), acRequest.getPrice());
+//            runningMap.put(acRequest.getUserId(), acThread);
+//        }
+//
+//        // 然后满足运行空调档位最高
+//        // 令人深思的调度方式, 使我的大脑旋转
+//        boolean flag = true;
+//        while (runningMap.size() <= acProperties.getCount() && !requestQueue.isEmpty() && flag) {
+//            flag = false;
+//            ACRequest acRequest = requestQueue.peek();
+//            Set<String> keys = runningMap.keySet();
+//            for (String userId : keys) {
+//                ACThread acThread = runningMap.get(userId);
+//                if (acThread == null) {
+//                    continue;
+//                }
+//                if (new BigDecimal(acThread.getPrice())
+//                        .compareTo(new BigDecimal(acRequest.getPrice())) < 0) {
+//                    flag = true;
+//                    requestQueue.poll();
+//
+//                    // 剥夺
+//                    runningMap.remove(userId);
+//                    ACRequest oldRequest = acThread.turnOffInSchedule();
+//                    if (oldRequest != null) {
+//                        addUniqueQueue(oldRequest, requestQueue);
+//                        acThread.setReq(oldRequest);
+//                        acThread.setStatus(ACStatus.WAITING);
+//                    }
+////                    log.info("old request: {}", oldRequest);
+////                    requestQueue.add(oldRequest);
+//
+//                    // 占用
+//                    ACThread ready = (ACThread) coolService.getACThread(acRequest.getUserId());
+//                    ready.turnOn(acRequest.getTargetTemperature(), acRequest.getChangeTemperature(),
+//                            acRequest.getStatus(), acRequest.getPrice());
+//                    runningMap.put(acRequest.getUserId(), ready);
+//                    break;
+//                }
+//            }
+//        }
+//        log.info("=======================调度后 {}========================", traceId);
+//        log.info("运行map: {}, 运行的空调个数: {}", runningMap, runningMap.size());
+//        log.info("调度队列: {}, 等待的空调个数: {}", requestQueue, requestQueue.size());
+//    }
+
+    /**
+     * 双优先队列调度
+     */
+    public synchronized void newSchedule() {
         String traceId = IdUtil.getSnowflakeNextIdStr();
         log.info("=======================调度前 {}========================", traceId);
-        log.info("运行map: {}, 运行的空调个数: {}", runningMap, runningMap.size());
+        log.info("运行队列: {}, 运行的空调个数: {}", runningQueue, runningQueue.size());
         log.info("调度队列: {}, 等待的空调个数: {}", requestQueue, requestQueue.size());
         ACProperties acProperties = (ACProperties) cacheService.get(RedisKeys.AC_PROPERTIES, ACProperties.class);
         // 先满足让运行空调达到最大值
-        while (runningMap.size() < acProperties.getCount() && !requestQueue.isEmpty()) {
+        while (runningQueue.size() < acProperties.getCount() && !requestQueue.isEmpty()) {
             ACRequest acRequest = requestQueue.poll();
             ACThread acThread = (ACThread) coolService.getACThread(acRequest.getUserId());
             acThread.turnOn(acRequest.getTargetTemperature(), acRequest.getChangeTemperature(),
                     acRequest.getStatus(), acRequest.getPrice());
-            runningMap.put(acRequest.getUserId(), acThread);
+            addUniqueQueue(acRequest, runningQueue);
         }
 
         // 然后满足运行空调档位最高
         // 令人深思的调度方式, 使我的大脑旋转
         boolean flag = true;
-        while (runningMap.size() <= acProperties.getCount() && !requestQueue.isEmpty() && flag) {
+        while (runningQueue.size() <= acProperties.getCount() && !requestQueue.isEmpty() && flag) {
             flag = false;
-            ACRequest acRequest = requestQueue.peek();
-            Set<String> keys = runningMap.keySet();
-            for (String userId : keys) {
-                ACThread acThread = runningMap.get(userId);
-                if (new BigDecimal(acThread.getPrice())
-                        .compareTo(new BigDecimal(acRequest.getPrice())) < 0) {
-                    flag = true;
-                    requestQueue.poll();
+            ACRequest request = requestQueue.peek();
+            ACRequest running = runningQueue.peek();
 
-                    // 剥夺
-                    runningMap.remove(userId);
-                    ACRequest oldRequest = acThread.turnOffInSchedule();
-                    if (oldRequest != null) {
-                        addUniqueQueue(oldRequest, requestQueue);
-                        acThread.setReq(oldRequest);
-                        acThread.setStatus(ACStatus.WAITING);
-                    }
-//                    log.info("old request: {}", oldRequest);
-//                    requestQueue.add(oldRequest);
+            if (request != null && running != null
+                    && new BigDecimal(request.getPrice()).compareTo(new BigDecimal(running.getPrice())) < 0) {
+                // 可以调度
+                flag = true;
+                requestQueue.poll();
+                runningQueue.poll();
+                log.info("发生了调度, 正在运行的: {}, 等待被调度进来的: {}", running, request);
 
-                    // 占用
-                    ACThread ready = (ACThread) coolService.getACThread(acRequest.getUserId());
-                    ready.turnOn(acRequest.getTargetTemperature(), acRequest.getChangeTemperature(),
-                            acRequest.getStatus(), acRequest.getPrice());
-                    runningMap.put(acRequest.getUserId(), ready);
-                    break;
+                // 剥夺
+                runningQueue.removeIf((e) -> e.getUserId().equals(running.getUserId()));
+                ACThread acThread = (ACThread) coolService.getACThread(running.getUserId());
+                ACRequest oldRequest = acThread.turnOffInSchedule();
+                if (oldRequest != null) {
+                    addUniqueQueue(oldRequest, requestQueue);
+                    acThread.setReq(oldRequest);
+                    acThread.setStatus(ACStatus.WAITING);
                 }
+
+                // 占用
+                ACThread ready = (ACThread) coolService.getACThread(request.getUserId());
+                ready.turnOn(request.getTargetTemperature(), request.getChangeTemperature(),
+                        request.getStatus(), request.getPrice());
+                addUniqueQueue(request, runningQueue);
             }
         }
         log.info("=======================调度后 {}========================", traceId);
-        log.info("运行map: {}, 运行的空调个数: {}", runningMap, runningMap.size());
+        log.info("运行队列: {}, 运行的空调个数: {}", runningQueue, runningQueue.size());
         log.info("调度队列: {}, 等待的空调个数: {}", requestQueue, requestQueue.size());
     }
 
